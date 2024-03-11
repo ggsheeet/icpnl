7f9b46032d56662d8ef5dbfda65a7af39d859abe
Luis Gzz <ggcryptodoc@gmail.com>
2024-03-11 14:09:33 -0600

    added course carousel

--- components/CoursesComp.js
+++ components/CoursesComp.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import { useQuery } from '@apollo/client'
 import { GET_C_FIR, GET_C_SEC, GET_C_THI } from '@/lib/queries'
+import CourseSlider from './CourseSlider'
 
 const CoursesComp = () => {
 	const {
@@ -10,15 +11,11 @@ const CoursesComp = () => {
 		data: dataFIR
 	} = useQuery(GET_C_FIR)
 
-	const mediaFIR = dataFIR?.mediaItem || []
-
 	const {
 		loading: loadingSEC,
 		error: errorSEC,
 		data: dataSEC
 	} = useQuery(GET_C_SEC)
-
-	const mediaSEC = dataSEC?.mediaItem || []
 
 	const {
 		loading: loadingTHI,
@@ -26,8 +23,14 @@ const CoursesComp = () => {
 		data: dataTHI
 	} = useQuery(GET_C_THI)
 
+	const mediaFIR = dataFIR?.mediaItem || []
+	const mediaSEC = dataSEC?.mediaItem || []
 	const mediaTHI = dataTHI?.mediaItem || []
 
+	// Combine all media items into a single array
+	const mediaItems = [mediaFIR, mediaSEC, mediaTHI]
+
+	// Remove <p> tags from the description
 	const removePTags = (htmlString) => {
 		if (htmlString) {
 			return htmlString.replace(/<\/?p[^>]*>/g, '')
@@ -35,57 +38,31 @@ const CoursesComp = () => {
 		return ''
 	}
 
-	// Remove <p> tags from the description
-	const firDescription = removePTags(mediaFIR.caption)
-	const secDescription = removePTags(mediaSEC.caption)
-	const thiDescription = removePTags(mediaTHI.caption)
-
-	return (
-		<div className='flex items-center justify-center'>
-		<div className='flex flex-wrap items-center justify-center bg-brand-white-smoke w-[90vw] gap-[3vh] md:gap-[2vw] pt-[5%] md:pt-[2%] pb-[1.5%]'>
-			{mediaFIR && mediaFIR.altText && (
+	// Format media items for CourseSlider
+	const formattedItems = mediaItems
+		.filter((media) => media)
+		.map((media) => {
+			const description = removePTags(media.caption)
+			return (
 				<a
-					href={firDescription}
-					rel='noopMAG noreferrer'
+					key={media.id}
+					href={description}
+					rel='noopener noreferrer'
 					target='_blank'
+					className='flex justify-center px-[2.5%] py-[3%]'
 				>
 					<Image
-						src={mediaFIR.sourceUrl}
-						alt={mediaFIR.altText}
-						width={mediaFIR.mediaDetails?.width}
-						height={mediaFIR.mediaDetails?.height}
-                        className='object-contain rounded-lg w-[88vw] md:w-[40vw] lg:w-[30vw] max-w-[160px] md:max-w-[250px] lg:max-w-[360px]'
+						src={media.sourceUrl}
+						alt={media.altText}
+						width={media.mediaDetails?.width}
+						height={media.mediaDetails?.height}
+						className='object-contain rounded-lg w-full h-auto'
 						priority
 					/>
 				</a>
-			)}
-			{mediaSEC && mediaSEC.altText && (
-				<a href={secDescription} rel='noopener noreferrer' target='_blank'>
-					<Image
-						src={mediaSEC.sourceUrl}
-						alt={mediaSEC.altText}
-						width={mediaSEC.mediaDetails?.width}
-						height={mediaSEC.mediaDetails?.height}
-                        className='object-contain rounded-lg w-[88vw] md:w-[40vw] lg:w-[30vw] max-w-[160px] md:max-w-[250px] lg:max-w-[360px]'
-						priority
-					/>
-				</a>
-			)}
-			{mediaTHI && mediaTHI.altText && (
-				<a href={thiDescription} rel='noopener noreferrer' target='_blank'>
-					<Image
-						src={mediaTHI.sourceUrl}
-						alt={mediaTHI.altText}
-						width={mediaTHI.mediaDetails?.width}
-						height={mediaTHI.mediaDetails?.height}
-                        className='object-contain rounded-lg w-[88vw] md:w-[40vw] lg:w-[30vw] max-w-[160px] md:max-w-[250px] lg:max-w-[360px]'
-						priority
-					/>
-				</a>
-			)}
-		</div>
-		</div>
-	)
+			)
+		})
+	return <CourseSlider items={formattedItems} />
 }
 
 export default CoursesComp
