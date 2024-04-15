be2b6b08c58eeae18ea234a6725f5e267e0865bf
Luis Gzz <ggcryptodoc@gmail.com>
2024-04-14 18:26:40 -0600

    new metadata april15

--- pages/index.js
+++ pages/index.js
@@ -167,12 +167,12 @@ export default function Home() {
 				<link rel='icon' href='/icpnl-ico.svg' />
 				<meta
 					property='og:title'
-					content='¿El SAT te debe dinero? Así puedes saberlo'
+					content='¿Presentaste tu anual? Trabajadores se quejan del aplicativo del SAT'
 				/>
 				<meta property='og:description' content='Boletín Diario ICPNL' />
 				<meta property='og:type' content='website' />
 				<meta property='og:url' content='https://icpnl.vercel.app' />
-				<meta property='og:image' content='/debe.png' />
+				<meta property='og:image' content='/satax.png' />
 				<meta property='og:image:type' content='image/jpg' />
 				<meta property='og:image:width' content='1200' />
 				<meta property='og:image:height' content='630' />
