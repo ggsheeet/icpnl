0793b9b761de4c4d0094d198681396ef5bd4b986
Luis Gzz <ggcryptodoc@gmail.com>
2024-04-15 20:41:36 -0600

    new metadata + course

--- pages/index.js
+++ pages/index.js
@@ -167,12 +167,12 @@ export default function Home() {
 				<link rel='icon' href='/icpnl-ico.svg' />
 				<meta
 					property='og:title'
-					content='¿Presentaste tu anual? Trabajadores se quejan del aplicativo del SAT'
+					content='Aspectos a considerar al renovar el registro en el REPSE'
 				/>
 				<meta property='og:description' content='Boletín Diario ICPNL' />
 				<meta property='og:type' content='website' />
 				<meta property='og:url' content='https://icpnl.vercel.app' />
-				<meta property='og:image' content='/satax.png' />
+				<meta property='og:image' content='/postthumb.png' />
 				<meta property='og:image:type' content='image/jpg' />
 				<meta property='og:image:width' content='1200' />
 				<meta property='og:image:height' content='630' />
