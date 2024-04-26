7974ee0c3d09f5ccd21ee8a33a17c73c3a177320
Luis Gzz <ggcryptodoc@gmail.com>
2024-04-25 22:05:54 -0600

    new metadata + courses april26

--- pages/index.js
+++ pages/index.js
@@ -167,12 +167,12 @@ export default function Home() {
 				<link rel='icon' href='/icpnl-ico.svg' />
 				<meta
 					property='og:title'
-					content='Beneficios fiscales para zona libre de Chetumal'
+					content='Daño de empresas fantasma al erario público supera los 40 bdp'
 				/>
 				<meta property='og:description' content='Boletín Diario ICPNL' />
 				<meta property='og:type' content='website' />
 				<meta property='og:url' content='https://icpnl.vercel.app' />
-				<meta property='og:image' content='/thumbone.png' />
+				<meta property='og:image' content='/fantasma.png' />
 				<meta property='og:image:type' content='image/jpg' />
 				<meta property='og:image:width' content='1200' />
 				<meta property='og:image:height' content='630' />
