fb00b8c045a72f728f3f5078b4947bb7a0c8f3c9
Luis Gzz <ggcryptodoc@gmail.com>
2024-05-12 21:27:03 -0600

    new metadata + promotion may13

--- pages/index.js
+++ pages/index.js
@@ -167,12 +167,12 @@ export default function Home() {
 				<link rel='icon' href='/icpnl-ico.svg' />
 				<meta
 					property='og:title'
-					content='SAT recupera 109 mdp por juicios al cierre de marzo'
+					content='¿Pediste tu devolución? SAT regresará 37,310 millones de pesos'
 				/>
 				<meta property='og:description' content='Boletín Diario ICPNL' />
 				<meta property='og:type' content='website' />
 				<meta property='og:url' content='https://icpnl.vercel.app' />
-				<meta property='og:image' content='/og-image.png' />
+				<meta property='og:image' content='/thumbnail.png' />
 				<meta property='og:image:type' content='image/png' />
 				<meta property='og:image:width' content='1200' />
 				<meta property='og:image:height' content='630' />
