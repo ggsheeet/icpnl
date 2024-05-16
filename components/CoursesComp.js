52ff1701a77dee4f918a620762d02ed48ad10b5d
Luis Gzz <ggcryptodoc@gmail.com>
2024-05-15 18:59:50 -0600

    new courses arrangement

--- components/CoursesComp.js
+++ components/CoursesComp.js
@@ -60,10 +60,10 @@ const CoursesComp = () => {
 		data: dataNIN
 	} = useQuery(GET_C_NIN)
 
-	const mediaFIR = dataFIR?.mediaItem || []
-	const mediaSEC = dataSEC?.mediaItem || []
-	const mediaTHI = dataTHI?.mediaItem || []
-	const mediaFOU = dataFOU?.mediaItem || []
+	// const mediaFIR = dataFIR?.mediaItem || []
+	// const mediaSEC = dataSEC?.mediaItem || []
+	// const mediaTHI = dataTHI?.mediaItem || []
+	// const mediaFOU = dataFOU?.mediaItem || []
 	const mediaFIV = dataFIV?.mediaItem || []
 	const mediaSIX = dataSIX?.mediaItem || []
 	const mediaSEV = dataSEV?.mediaItem || []
@@ -71,9 +71,9 @@ const CoursesComp = () => {
 	// const mediaNIN = dataNIN?.mediaItem || []
 
 	// Combine all media items into a single array
-	const mediaItems = [mediaFIR, mediaSEC, mediaTHI, mediaFOU, mediaFIV, mediaSIX, mediaSEV]
+	const mediaItems = [mediaFIV, mediaSIX, mediaSEV]
 
-	//  , mediaEIG, mediaNIN
+	//  mediaFIR, mediaSEC, mediaTHI, mediaFOU, , , mediaEIG, mediaNIN
 
 	// Remove <p> tags from the description
 	const removePTags = (htmlString) => {
@@ -109,8 +109,8 @@ const CoursesComp = () => {
 		})
 	return (
 		<>
-		{/*  | loadingEIG | loadingNIN */}
-			{ loadingFIR | loadingSEC | loadingTHI | loadingFOU | loadingFIV | loadingSIX | loadingSEV ? (
+		{/* loadingFIR | loadingSEC | loadingTHI | loadingFOU |   | loadingEIG | loadingNIN */}
+			{ loadingFIV | loadingSIX | loadingSEV ? (
 				Array.from({ length: 1 }).map((_, index) => (
 					<div key={index} className='p-4'>
 						<Skeleton
